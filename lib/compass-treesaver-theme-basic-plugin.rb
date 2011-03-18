116a58c5aeabd3f5eaefc5c0bcf72ce6a9c19e04
Andrea Campi <andrea.campi@zephirworks.com>
2011-03-18 15:11:23 +0100

    First code drop.

---
+++ lib/compass-treesaver-theme-basic-plugin.rb
@@ -0,0 +1,3 @@
+require 'compass'
+
+Compass::Frameworks.register("treesaver-theme-basic", :path => File.expand_path("..", File.dirname(__FILE__)))
---
+++ templates/project/manifest.rb
@@ -0,0 +1,1 @@
+stylesheet '_compass_treesaver_theme_default.scss', :media => 'screen'
